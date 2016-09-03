399bed721da75462c7f232dda02a93affec6faec
Joshua Fingert <joshuafingert@gmail.com>
2016-09-03 07:59:21 -0700

    remove config require

--- bin/bot.js
+++ bin/bot.js
@@ -8,13 +8,12 @@ var apiToken = process.env.apiToken;
 var forecastApiKey = process.env.forecastApiKey;
 var googleApiKey = process.env.googleApiKey;
 var weatherBotUserName = process.env.weatherBotUserName;
-var config = require('../config') || {};
 
 var weatherBot = new WeatherBot({
-    token: config.apiToken || apiToken,
-    botName: config.weatherBotUserName || weatherBotUserName,
-    forecastApiKey: config.forecastApiKey || forecastApiKey,
-    googleApiKey: config.googleApiKey || googleApiKey
+    token: apiToken,
+    botName: weatherBotUserName,
+    forecastApiKey: forecastApiKey,
+    googleApiKey: googleApiKey
 });
 
 weatherBot.run();
