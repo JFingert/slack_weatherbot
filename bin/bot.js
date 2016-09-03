aca93973fdab44647c1cbf69bba6dc76d16cae64
Joshua Fingert <joshuafingert@gmail.com>
2016-09-02 21:55:10 -0700

    remove comment

--- bin/bot.js
+++ bin/bot.js
@@ -4,14 +4,6 @@
 
 var WeatherBot = require('../lib/weatherbot');
 
-/**
- * Environment variables used to configure the bot:
- *
- *  BOT_API_KEY : the authentication token to allow the bot to connect to your slack organization. You can get your
- *      token at the following url: https://<yourorganization>.slack.com/services/new/bot (Mandatory)
- *  BOT_DB_PATH: the path of the SQLite database used by the bot
- *  BOT_NAME: the username you want to give to the bot within your organisation.
- */
 var config = require('../config');
 
 var weatherBot = new WeatherBot({
